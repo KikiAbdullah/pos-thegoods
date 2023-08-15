edd7ee33ed60598e9ecbacfdd37ed0e56356f24c
nizarhdt <nizar.hdt@gmail.com>
2023-08-15 14:55:29 +0700

    2fa blade & numbertrait

--- app/Http/Controllers/Traits/NumberTrait.php
+++ app/Http/Controllers/Traits/NumberTrait.php
@@ -5,6 +5,86 @@ namespace App\Http\Controllers\Traits;
 use DB;
 
 trait NumberTrait {
+    public function gen_number_with_where_in($model, $column, $prefix, $date, $field_date, $every_month = false, $whereCol, $whereVal)
+	{
+        // $prefix = JN$$-@@######
+        // A-Z = String
+        // $ = Year of date
+        // @ = Month of date
+        // # = Increment
+        // % = Month of romman
+        $year ="";
+        $month = "";
+        if(!empty($date)){
+            $year = date("Y",strtotime($date));
+            $month = date("m",strtotime($date));
+        }
+
+        $jml = substr_count($prefix, '#');
+        $increment = str_repeat("#", $jml);
+        $pos = strpos($prefix, "#");
+        $adaY = strpos($prefix, "$");
+        $adaM = strpos($prefix, "@");
+        //check model
+        if(!empty($date)){
+        	$lastno = $model::withTrashed()->select(DB::raw('IFNULL(MAX(CAST(SUBSTRING('.$column.', '.($pos+1).','.$jml.') as UNSIGNED)),0) * 1 AS idmax'))->whereIn($whereCol, $whereVal);
+			$lastno = $this->queryParams($lastno, ['adaY' => $adaY, 'adaM' => $adaM, 'every_month' => $every_month, 'field_date' => $field_date, 'year' => $year, 'month' => $month]);
+			$lastno = $lastno->first()->idmax;
+        }else{
+            $lastno = $model::withTrashed()->select(DB::raw('IFNULL(MAX(CAST(SUBSTRING('.$column.', '.($pos+1).','.$jml.') as UNSIGNED)),0) * 1 AS idmax'))->whereIn($whereCol, $whereVal)
+                                        ->first()->idmax;
+        }
+
+        $count_dollar = substr_count($prefix, '$');
+
+        $nextno = str_pad($lastno+1, $jml, "0", STR_PAD_LEFT);
+        $result = str_replace($increment,$nextno,$prefix);
+        $result = str_replace(str_repeat('$', $count_dollar), substr($year, $count_dollar == 2 ? 2 : 0, $count_dollar),$result);
+        $result = str_replace('@@',substr($month,0,2),$result);
+        $result = str_replace('%%', $this->numberToRoman(date('m',strtotime($date))), $result);
+        return $result;
+    }
+
+    public function gen_number_with_where($model, $column, $prefix, $date, $field_date, $every_month = false, $whereCol, $whereVal)
+	{
+        // $prefix = JN$$-@@######
+        // A-Z = String
+        // $ = Year of date
+        // @ = Month of date
+        // # = Increment
+        // % = Month of romman
+        $year ="";
+        $month = "";
+        if(!empty($date)){
+            $year = date("Y",strtotime($date));
+            $month = date("m",strtotime($date));
+        }
+
+        $jml = substr_count($prefix, '#');
+        $increment = str_repeat("#", $jml);
+        $pos = strpos($prefix, "#");
+        $adaY = strpos($prefix, "$");
+        $adaM = strpos($prefix, "@");
+        //check model
+        if(!empty($date)){
+        	$lastno = $model::withTrashed()->select(DB::raw('IFNULL(MAX(CAST(SUBSTRING('.$column.', '.($pos+1).','.$jml.') as UNSIGNED)),0) * 1 AS idmax'))->where($whereCol, $whereVal);
+			$lastno = $this->queryParams($lastno, ['adaY' => $adaY, 'adaM' => $adaM, 'every_month' => $every_month, 'field_date' => $field_date, 'year' => $year, 'month' => $month]);
+			$lastno = $lastno->first()->idmax;
+        }else{
+            $lastno = $model::withTrashed()->select(DB::raw('IFNULL(MAX(CAST(SUBSTRING('.$column.', '.($pos+1).','.$jml.') as UNSIGNED)),0) * 1 AS idmax'))->where($whereCol, $whereVal)
+                                        ->first()->idmax;
+        }
+
+        $count_dollar = substr_count($prefix, '$');
+
+        $nextno = str_pad($lastno+1, $jml, "0", STR_PAD_LEFT);
+        $result = str_replace($increment,$nextno,$prefix);
+        $result = str_replace(str_repeat('$', $count_dollar), substr($year, $count_dollar == 2 ? 2 : 0, $count_dollar),$result);
+        $result = str_replace('@@',substr($month,0,2),$result);
+        $result = str_replace('%%', $this->numberToRoman(date('m',strtotime($date))), $result);
+        return $result;
+    }
+    
     public function gen_number($model, $column, $prefix, $date, $field_date, $every_month = false)
 	{
         // $prefix = JN$$-@@######
--- resources/views/auth/two_factor.blade.php
+++ resources/views/auth/two_factor.blade.php
@@ -1,47 +1,57 @@
-@php
-$value = Cookie::get('nhTheme');
-if (empty($value)) {
-    $theme = 'default';
-} else {
-    $theme = 'dark';
-}
-@endphp
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Two Factor Auth - {{ env('APP_NAME') }}</title>
-    @if ($theme == 'default')
-        <link rel="icon" href="{{ asset('global_assets/images/logo_favicon_dark.png') }}" sizes="32x32" />
-        <link rel="icon" href="{{ asset('global_assets/images/logo_favicon_dark.png') }}" sizes="192x192" />
-        <link rel="shortcut apple-touch-icon" href="{{ asset('global_assets/images/logo_favicon_dark.png') }}">
-        <meta name="msapplication-TileImage" content="{{ asset('global_assets/images/logo_favicon_dark.png') }}" />
-    @else
-        <link rel="icon" href="{{ asset('global_assets/images/logo_favicon_light.png') }}" sizes="32x32" />
-        <link rel="icon" href="{{ asset('global_assets/images/logo_favicon_light.png') }}" sizes="192x192" />
-        <link rel="shortcut apple-touch-icon" href="{{ asset('global_assets/images/logo_favicon_light.png') }}">
-        <meta name="msapplication-TileImage" content="{{ asset('global_assets/images/logo_favicon_light.png') }}" />
-    @endif
 
     <!-- Global stylesheets -->
-    <link href="{{ asset('global_assets/css/fontnya.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('global_assets/css/icons/icomoon/styles.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset($theme . '/assets/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/fonts/inter/inter.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/icons/phosphor/styles.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/css/ltr/all.min.css') }}" id="stylesheet" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/logo-font.css') }}" id="stylesheet" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/style.css') }}" id="stylesheet" rel="stylesheet" type="text/css">
     <!-- /global stylesheets -->
+
+    <!-- Core JS files -->
+    <script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
+    <!-- /core JS files -->
+
+    <!-- Theme JS files -->
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+    <script src="{{ asset('app_local/js/theme.js') }}"></script>
+    <!-- /theme JS files -->
     <style>
-        input {
-            letter-spacing: 8px;
-            word-spacing: 16px;
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: top;
+            margin-bottom: 40px;
         }
 
+        .input {
+            width: 50px;
+            border: none;
+            border: 3px solid rgba(0, 0, 0, 0.1);
+            margin: 0 6px;
+            text-align: center;
+            font-size: 36px;
+            border-radius: 12px;
+        }
+
+        .input:focus {
+            border: 3px solid orange;
+            outline: none;
+        }
+
+        .input:nth-child(1) {
+            cursor: pointer;
+            pointer-events: all;
+        }
     </style>
-    <!-- Core JS files -->
-    <script src="{{ asset('global_assets/js/main/jquery.min.js') }}"></script>
-    <script src="{{ asset('global_assets/js/main/bootstrap.bundle.min.js') }}"></script>
-    <!-- /core JS files -->
 
 </head>
 
@@ -64,11 +74,7 @@ if (empty($value)) {
                     <div class="card mb-0">
                         <div class="card-body">
                             <div class="text-center">
-                                <div class="card-img-actions d-inline-block mb-3">
-                                    <img class="rounded-circle"
-                                        src="{{ asset('global_assets/images/placeholders/placeholder.jpg') }}"
-                                        width="160" height="160" alt="Avatar">
-                                </div>
+                                <i class="ph-lock ph-3x"></i>
                             </div>
 
                             <div class="text-center mb-3">
@@ -95,16 +101,21 @@ if (empty($value)) {
                             </div>
 
                             <div class="form-group">
-                                <input type="text" class="form-control text-center font-weight-bold" name="otp"
-                                    placeholder="______" autofocus>
+                                <div class="container">
+                                    <div id="inputs" class="inputs">
+                                        <input class="input" type="text" inputmode="numeric" maxlength="1" autofocus />
+                                        <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                                        <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                                        <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                                        <input class="input" type="text" inputmode="numeric" maxlength="1" />
+                                    </div>
+                                </div>
+                                <input type="hidden" name="otp">
                                 <input type="hidden" name="redirect" value="{{ request()['redirect'] }}">
                             </div>
 
-                            <button type="submit" class="btn btn-primary btn-block"><i class="icon-unlocked mr-2"></i>
-                                Confirm</button>
-                            <a href="#!"
-                                onclick="event.preventDefault(); document.getElementById('frm-logout').submit();"
-                                class="btn btn-secondary btn-block"> Back to Login</a>
+                            <button type="submit" class="btn btn-primary w-100 mb-2"><i class="ph-lock-simple-open mr-4"></i>&nbsp;Confirm</button>
+                            <a href="#!" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();" class="btn btn-light w-100"> Back to Login</a>
                         </div>
                     </div>
                     <!-- /unlock form -->
@@ -124,7 +135,53 @@ if (empty($value)) {
 
     </div>
     <!-- /page content -->
+    <script type="text/javascript">
+        const inputs = document.getElementById("inputs");
 
+        inputs.addEventListener("input", function(e) {
+            const target = e.target;
+            const val = target.value;
+
+            if (isNaN(val)) {
+                target.value = "";
+                return;
+            }
+
+            if (val != "") {
+                const next = target.nextElementSibling;
+                if (next) {
+                    next.value = "";
+                    next.focus();
+                }
+            }
+        });
+
+        inputs.addEventListener("keyup", function(e) {
+            const target = e.target;
+            const key = e.key.toLowerCase();
+
+            if (key == "backspace" || key == "delete") {
+                target.value = "";
+                const prev = target.previousElementSibling;
+                if (prev) {
+                    prev.focus();
+                }
+                return;
+            }
+        });
+
+        $(function() {
+            $("body").on("submit", ".login-form", function(e) {
+                const inputs = document.querySelectorAll("input.input");
+                let otp = "";
+                inputs.forEach(el => {
+                    otp += el.value;
+                });
+                $('input[name="otp"]').val(otp);
+                return true;
+            });
+        });
+    </script>
 </body>
 
 </html>
